623aaf4413fcbd411b16017913e40b8abe9e28db
limin <1512510162@qq.com>
2018-11-29 09:15:08 +0800

    Initial Commit

---
+++ LMShoppingCart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMShoppingCart
+//
+//  Created by Apple on 2018/11/29.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
